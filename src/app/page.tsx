6eaf10e6cbb69253e7940e0a29eee2f326d96397
Mariano Guillaume <marianguillaume.m@gmail.com>
2025-02-07 23:22:02 -0300

    New landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      This is a starter for Next.js with Tailwind CSS (with App router)
+    <main className='flex min-h-screen flex-col items-center justify-center p-24 gap-8'>
+      <h1 className='text-4xl font-bold'>This is a starter for <span className="text-blue-500">Next.js</span> with <span className="text-sky-400">Tailwind CSS</span> (with App router)</h1>
     </main>
   );
 }
