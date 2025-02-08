e0f9bdf52dc8c62027c41d8ed8047b83ea3ca49b
Mariano Guillaume <marianguillaume.m@gmail.com>
2025-02-07 23:09:35 -0300

    Refactor Tailwind CSS integration and update project structure

---
+++ src/components/folder.info.ts
@@ -0,0 +1,4 @@
+/**
+ * In this folder we will create all the components that will be used in the application.
+ * 
+ */
---
+++ src/models/folder.info.ts
@@ -0,0 +1,4 @@
+/**
+ * In this folder we will create all the models and business rules that will be used in the application.
+ * 
+ */
